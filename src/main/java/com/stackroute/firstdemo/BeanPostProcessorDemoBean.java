12978e6a10e7e4828603887eb5a19baf9939a92d
shubham.5.srivastava <shubham.5.srivastava@niit.com>
2019-04-29 12:33:40 +0530

    update code

---
+++ src/main/java/com/stackroute/firstdemo/BeanPostProcessorDemoBean.java
@@ -0,0 +1,20 @@
+package com.stackroute.firstdemo;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+public class BeanPostProcessorDemoBean implements BeanPostProcessor {
+
+	@Override
+	public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+		System.out.println("Before initialization " +beanName);
+		return bean;
+	}
+
+	@Override
+	public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+		System.out.println("After initialization " +beanName);
+		return bean;
+	}
+
+}
