257042fe2fed1e302d26eb62ebf00494a7e73cfc
shubham.5.srivastava <shubham.5.srivastava@niit.com>
2019-04-29 11:51:41 +0530

    update code

--- src/main/java/com/stackroute/firstdemo/Movie.java
+++ src/main/java/com/stackroute/firstdemo/Movie.java
@@ -1,5 +1,13 @@
 package com.stackroute.firstdemo;
-public class Movie {
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class Movie implements ApplicationContextAware, BeanFactoryAware, BeanNameAware {
  Actor actor;
 
 
@@ -16,5 +24,26 @@ public void check() {
 public Movie(Actor actor) {
 	super();
 	this.actor = actor;
+}
+
+@Override
+public void setBeanName(String name) {
+	// TODO Auto-generated method stub
+	System.out.println("the bean name is "+name);
+	
+}
+
+@Override
+public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+	// TODO Auto-generated method stub
+	System.out.println("the beanfatory is "+beanFactory);
+	
+}
+
+@Override
+public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+	// TODO Auto-generated method stub
+	System.out.println("the application context is "+applicationContext);
+	
 }
 }
